f591944c6f6bdfa09c3f2ea387cffb7897372a4f
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-10-26 23:23:58 +0100

    ⚙ Setting up login / register handlers

---
+++ utils/login-handler.php
@@ -0,0 +1,75 @@
+<?php
+include_once 'config.php';
+$loginspace = True;
+
+// Session init
+if (session_status() == PHP_SESSION_NONE)
+{
+    session_start();
+}
+
+// Message
+$errorMessages = [];
+$successMessages = [];
+
+// Base values
+$login = '';
+$password = '';
+
+// Form sent
+if(!empty($_POST))
+{
+
+    // Sanitize data
+    $login = trim(strip_tags($_POST['login']));
+    $password = $_POST['password'];
+
+    // Errors
+    if(empty($login))
+    {
+        $errorMessages[] = 'Missing login';
+    }
+    if(empty($password))
+    {
+        $errorMessages[] = 'Missing password';
+    }
+    else if(strlen($password) < 5)
+    {
+        $errorMessages[] = 'Password too short';
+    }
+
+    // Success
+    if(empty($errorMessages))
+    {
+        $requser = $pdo->prepare('SELECT * FROM users WHERE (login = ? OR mail = ?)');
+        $requser->execute(array($login, $login));
+        $userexist = $requser->rowCount();
+        if($userexist == 1)
+        {
+            $userinfo = $requser->fetch();
+            $passwordhash = $userinfo['password'];
+            if (password_verify($password, $passwordhash))
+            {
+                $_SESSION['login'] = $userinfo['login'];
+                $_SESSION['id'] = $userinfo['id'];
+                $_SESSION['admin'] = $userinfo['admin'];
+                $_SESSION['mail'] = $userinfo['mail'];
+                $_SESSION['avatar'] = $userinfo['avatar'];
+                $_SESSION['auth'] = $userinfo;
+                $successMessages[] = 'You are now logged in!';
+                // Reset values
+                $login = '';
+                $password = '';
+                header('Location: index.php');   
+            }
+            else
+            {
+                $errorMessages[] = 'Wrong login or password.';
+            }
+        }
+        else
+        {
+            $errorMessages[] = 'Wrong login or password.';
+        }
+    }
+}
---
+++ utils/register-handler.php
@@ -0,0 +1,96 @@
+<?php
+
+// Message
+$errorMessages = [];
+$successMessages = [];
+
+// Base values
+$login = '';
+$mail = '';
+$password = '';
+
+
+// Form sent
+if(!empty($_POST))
+{
+    // Sanitize data
+    $login = trim(strip_tags($_POST['login']));
+    $password = $_POST['password'];
+    $passwordconfirm = $_POST['passwordconfirm'];
+    $mail = $_POST['mail'];
+
+    // Errors
+    if(empty($login))
+    {
+        $errorMessages[] = 'Missing login';
+    }
+    if(empty($mail))
+    {
+        $errorMessages[] = 'Missing mail';
+    }
+    else if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+    {
+        $errorMessages[] = 'Wrong mail format';
+    }
+    if(empty($password))
+    {
+        $errorMessages[] = 'Missing password';
+    }
+    else if(strlen($password) < 5)
+    {
+        $errorMessages[] = 'Password too short';
+    }
+    else if(!preg_match('/[a-z]/', $password) || !preg_match('/[A-Z]/', $password) || !preg_match('/[0-9]/', $password))
+    {
+        $errorMessages[] = 'Password should contain lowercase, uppercase and numbers';
+    }
+    else if($password != $passwordconfirm)
+    {
+        $errorMessages[] = 'Password must be the same !';
+    }
+
+
+    // Check DB and Success
+    if(empty($errorMessages))
+    {
+        $reqlogin = $pdo->prepare("SELECT * FROM users WHERE login = ?");
+        $reqlogin->execute(array($login));
+        $loginexist = $reqlogin->rowCount();
+        if($loginexist == 0)
+        {
+            $reqmail = $pdo->prepare("SELECT * FROM users WHERE mail = ?");
+            $reqmail->execute(array($mail));
+            $mailexist = $reqmail->rowCount();
+            if($mailexist == 0)
+            {
+                $successMessages[] = 'You are now registered  <a href="login.php"">Click here to connect</a>';
+        
+                // DB Add values
+                $prepare = $pdo->prepare('
+                    INSERT INTO
+                        users (login, password, mail)
+                    VALUES
+                        (:login, :password, :mail)
+                ');
+
+                $prepare->bindValue(':login', $login);
+                $prepare->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
+                $prepare->bindValue(':mail', $mail);
+                $prepare->execute();
+            }
+            else
+            {
+                $errorMessages[] = 'This mail is already used !';
+            }
+        }
+        else 
+        {
+            $errorMessages[] = 'This login is already used !';
+        }
+
+        // Reset values
+        $login = '';
+        $password = '';
+        $mail = '';
+    }
+}
